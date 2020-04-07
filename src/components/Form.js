afffad8701d9d58a44b2f188307243806f886b29
Sthefany Azevedo <Sthefany@Sthefanys-MacBook-Pro-4.local>
2020-04-06 20:01:05 -0400

    Update venues

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,47 +3,58 @@ import axios from 'axios';
 
 class Form extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      venues: [],
+      lat: 0,
+      lng: 0
+    };
+  }
 
-    constructor() {
-    
-        super();
-    
-        this.state = {
-          venues: []
-        };
-      }
-    }
+  componentDidMount() {
+      this.getVenues();
+  }
 
-    componentDidMount() {
-        this.getVenues();
-      }
-    
-    getVenues() {
+  getVenues() {
 
-        let setVenueState = this.setState.bind(this);
+    let setVenueState = this.setState.bind(this);
 
-        const venuesEndpoint = 'https://api.foursquare.com/v2/venues/search';
+    const venuesEndpoint = 'https://api.foursquare.com/v2/venues/search';
 
-        const params = {
-        client_id: "QFWFOQW13VWR2SYCHO3SLUWQDDDDRAC3ZA1RSH2BOY5LBVOG",
-        client_secret: "OEBLZFQVFD1M3UTY4GCI4M42DWXC14COGEW0F3LGWJZ4ST2Z",
-        limit: 20, //The max number of venues to load
-        query: 'query', //The type of venues we want to query
-        v: '20200403', //The version of the API.
-        ll: '40.74224,-73.99386' //The latitude and longitude 
-        };
+    const params = {
+      client_id: "QFWFOQW13VWR2SYCHO3SLUWQDDDDRAC3ZA1RSH2BOY5LBVOG",
+      client_secret: "OEBLZFQVFD1M3UTY4GCI4M42DWXC14COGEW0F3LGWJZ4ST2Z",
+      limit: 20, //The max number of venues to load
+      query: 'query', //The type of venues we want to query
+      v: '20220403', //The version of the API.
+      ll: '25.761681, -80.191788' //The latitude and longitude
+    };
 
 
     fetch(venuesEndpoint + new URLSearchParams(params), {
         method: 'GET'
-      }).then(response => response.json()).then(response => {
-        this.setState({venues: response.response.groups[0].items}); //Set the components state
+      })
+        .then(response => response.json())
+        .then(response => {
+          this.setState({venues: response.response.venues}); //Set the components state
       });
+  }
 
 
   render() {
     return (
-      <div></div>
+      <div>
+          <ul>
+            {
+              this.state.venues.map((venues, index) => {
+                return (
+                  <li key={}></li>
+                )
+              })
+            }
+          </ul>
+      </div>
     );
   }
 }
