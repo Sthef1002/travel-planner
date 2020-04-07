464b5021903dbe1aa1c3245a0a0c848b13b9dddd
Sthefany Azevedo <Sthefany@Sthefanys-MacBook-Pro-4.local>
2020-04-07 18:06:18 -0400

    Add Jumbotron, Move Backgroun.png to public, Add css properties

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import { Route, BrowserRouter } from 'react-router-dom';
 import React, { useState } from 'react';
 import Form from './components/Form';
 import Search from './components/Search';
 import Map from './components/Map';
-import { Route, BrowserRouter } from 'react-router-dom';
 import Results from './components/Results';
 import Logo from './components/Logo';
 import Home from "./components/Home";
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Form from './Form';
-import './Home.css'
- 
+import './Home.css';
+import Jumbotron from 'react-bootstrap/Jumbotron'; 
+import Container from 'react-bootstrap/Container'; 
+
+
 const home = () => {
     return (
         <div>
-            <h1>For those who seek</h1>
-            <h2>Find places to eat, drink, shop, or visit in any city in the world, add them to your trip 
-planner and get ready to explore.</h2>
-            <p>Travel Planner © 2020 All rights reserved.</p>
-            <img id='img-back' src='./Background.png' alt='background'></img>
+            <Jumbotron fluid id='home-back'>
+                <Container>
+                    <h1>For those who seek</h1>
+                    <h2>Find places to eat, drink, shop, or visit in any city in the world, add them to your trip 
+        planner and get ready to explore.</h2>
+                    <Form />
+                    <p id='copyright'>Travel Planner © 2020 All rights reserved.</p>
+                </Container>
+            </Jumbotron>
             <Form />
         </div>
     );
