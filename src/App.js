7f2c9d18c0fb057199f24391a7510dbc1c719804
Sthefany Azevedo <Sthefany@Sthefanys-MacBook-Pro-4.local>
2020-04-06 20:44:04 -0400

    'Add Search.js component'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Form from './components/Form'
+import Search from './components/Search'
+import Map from './components/Map'
+
+
 
 function App() {
   return (
     <div>
       <Form />
-      {/* <Map /> */}
+      <Map />
       {/* <MapCard /> */}
       {/* <CardList /> */}
       {/* <DateList /> */}
@@ -12,6 +17,7 @@ function App() {
       {/* <Logo /> */}
       {/* <NavBar /> */}
       {/* <Details /> */}
+      <Search />
     </div>
   );
 }
