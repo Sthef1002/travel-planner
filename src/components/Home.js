45c357a7d87c8411d7ce2d873d95b783255cf019
Israel Martinez <marcemc@yahoo.com>
2020-04-08 15:18:59 -0400

    Fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Form from './Form';
 import Logo from './Logo';
 import './Home.css';
-import Jumbotron from 'react-bootstrap/Jumbotron'; 
-import Container from 'react-bootstrap/Container'; 
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 import Search from './Search';
+import { Route } from 'react-router-dom'
 
 
 const home = () => {
@@ -14,15 +15,15 @@ const home = () => {
                 <Container>
                     <Logo />
                     <h1 id='title'>For those who seek</h1>
-                    <h2 id='subTitle'>Find places to eat, drink, shop, or visit in any city in the world, add them to your trip 
+                    <h2 id='subTitle'>Find places to eat, drink, shop, or visit in any city in the world, add them to your trip
                     planner and get ready to explore.</h2>
                     <Form />
-                    <Search />
+                    <Route exact path="/" component={Search} />
                     <p id='copyright' >Travel Planner © 2020 All rights reserved.</p>
                 </Container>
             </Jumbotron>
         </div>
     );
 }
- 
+
 export default home;
