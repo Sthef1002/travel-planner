e7877415c45ee8a51b4ab65536bb5a9266214e20
Sthefany Azevedo <Sthefany@Sthefanys-MacBook-Pro-4.local>
2020-04-12 18:38:42 -0400

    try to obtain data for details

--- src/components/Details.js
+++ src/components/Details.js
@@ -23,16 +23,16 @@ export default class Details extends Component {
                     {venueDetails.name}
                 </div>
                 <div id='venue-cat'>
-                    {venueDetails.name}
+                    {/* {venueDetails.categories[0].name} */}
                 </div>
                 <div id='venue-det'>
                     {/* Central Park is the 843-acre green heart of Manhattan and is maintained by the Central Park Conservancy. It was designed in the 19th century by Frederick Law Olmsted and Calvert Vaux as an urban escape for New Yorkers, and now receives over 40 million visits per year */}
                 </div>
                 <div>
-                    {venueDetails.address}
+                    {venueDetails.rating}
                 </div>
                 <div>
-                    {/* Hours */}
+                    {/* {venueDetails.contact[0].phone} */}
 
                 </div>
 
