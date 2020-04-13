d73721c8d172bdbeeb5bc150182161e9940b2254
Sthefany Azevedo <Sthefany@Sthefanys-MacBook-Pro-4.local>
2020-04-13 17:09:09 -0400

    Remove cardMap from App.js, Add button to go 'back' and 'add to list +' to comopnent Details.js, Syle Details #modal

--- src/components/Details.js
+++ src/components/Details.js
@@ -9,54 +9,63 @@ export default class Details extends Component {
         return (
 
             <section id='modal'>
-                {/* <button
-                  onClick={() => this.props.changeSelections(this.props.venueDetails)}
-                >
-                  Add to list
-                </button> */}
-                <div id='venue-img'>
-                  {
-                    venueDetails.bestPhoto && (
-                      <img
-                        src={venueDetails.bestPhoto.prefix+'300x300'+venueDetails.bestPhoto.suffix}
-                        alt={`${venueDetails.name} Image`}
-                        style={{maxWidth: '100%'}}
-                      />
-                    )
-                  }
-                </div>
-                <div id='venue-name'>
-                    {venueDetails.name}
-                </div>
-                <div id='venue-cat'>
-                    { venueDetails.categories && venueDetails.categories[0].name }
-                </div>
-                <div id='venue-des'>
-                    {venueDetails.description}
-                </div>
-                <div id='venue-ll'>
-                    {venueDetails.location && venueDetails.location.formattedAddress}
-                </div>
-                <div id='venue-hours'>
-                  {
-                    venueDetails.hours &&
-                    venueDetails.hours.timeframes.map( tf => (
-                      <Fragment>
-                        <div>
-                          Days: {tf.days}
-                        </div>
-                        <div >
-                          Hours:
-                          {
-                            tf.open.map( time => (
-                              time.renderedTime
-                            ))
-                          }
-                        </div>
-                      </Fragment>
-                    ))
-                  }
-                </div>
+              <button id='back'> 
+              Back              
+              </button>
+                <button
+                id='add'
+                onClick={() => this.props.changeSelections(this.props.venueDetails)}
+              >
+                Add to list +
+              </button>
+              <div id='venue-img'>
+                {
+                  venueDetails.bestPhoto && (
+                    <img 
+                      src={venueDetails.bestPhoto.prefix+'560x200'+venueDetails.bestPhoto.suffix}
+                      alt={`${venueDetails.name} Image`}
+                      style={{maxWidth: '100%'}}
+                    />
+                  )
+                }
+              </div>
+              <div id='venue-name'>
+                  {venueDetails.name}
+              </div>
+              <div id='venue-cat'>
+                  { venueDetails.categories && venueDetails.categories[0].name }
+              </div>
+              <div id='venue-des'>
+                  {venueDetails.description}
+              </div>
+              <div id='venue-ll'>
+                  <p id='sub'>
+                    Address
+                  </p>
+                  {venueDetails.location && venueDetails.location.formattedAddress}
+              </div>
+                  <p id='sub'>
+                    Hours 
+                  </p>
+              <div id='venue-hours'>
+                {
+                  venueDetails.hours &&
+                  venueDetails.hours.timeframes.map( tf => (
+                    <Fragment>
+                      <div>
+                      {tf.days}
+                      </div>
+                      <div>
+                        {
+                          tf.open.map( time => (
+                            time.renderedTime
+                          ))
+                        }
+                      </div>
+                    </Fragment>
+                  ))
+                }
+              </div>
             </section>
 
         )
