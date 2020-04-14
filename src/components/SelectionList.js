bc776261f663d4e68108a17bc5bca551ec26e617
carolunadsilver <carolinadsilver@gmail.com>
2020-04-14 18:35:43 -0400

    updates to selected list

--- src/components/SelectionList.js
+++ src/components/SelectionList.js
@@ -24,7 +24,7 @@ class SelectionList extends Component {
                             <Card.Img 
                               id='image-select'
                               variant="top" 
-                              src="https://picsum.photos/id/204/100/100/" 
+                              src={`https://maps.googleapis.com/maps/api/streetview?size=200x100&location=${venue.location.lat},${venue.location.lng}&fov=90&heading=235&pitch=10&key=AIzaSyDicFQoRWREO_gX1sTklPjE8Kjhni7qmQU`}
 
                             />
                             <Card.Body id='content-select'>
@@ -32,7 +32,7 @@ class SelectionList extends Component {
                                 {venue.name} 
                               </Card.Title>
                               <Card.Text id='description-select'>
-                                {venue.description}
+                                {venue.categories[0].name}
                               </Card.Text>
                             <Card.Link id='link-details' href='({venueId: venue.id})}'>
                               Show details
